cff99eb1fede0516b10db83cd79840fcabad673e
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-06-13 19:05:35 +0300

    main

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelector('toggle_button')
-const toggleBtnIcon = document.querySelector('icon')
-const dropDown = document.querySelector('dropdown')
-
-toggleBtn.onclick = function (){
-  dropDown.classList.toggle('open')
-}
